9d04c43711a2aa1e298564b329d6bf137bf3514a
SHIFANA <shifana0414@gmail.com>
2024-08-27 22:31:44 +0530

    sample

---
+++ jenkinsproject/src/main/java/jenkinsPackage/Sample.java
@@ -0,0 +1,21 @@
+package jenkinsPackage;
+
+public class Sample 
+{
+	public void add()
+	{
+		 int a=30, b=10;
+		 int s=a+b;
+		 System.out.println(s);
+	}
+
+
+	public static void main(String[] args)
+	{
+		Sample obj=new Sample();
+		obj.add();
+		
+
+	}
+
+}
